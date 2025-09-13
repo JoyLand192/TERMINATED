9e7b1a453fba4909a86e1beeb91272f763ff2f32
금마리 <cyw46421@gmail.com>
2025-09-13 17:28:22 +0900

    added move tutorial
    ez gugugaga

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
     public CR cr;
+
+    [SerializeField] CanvasGroup tutorialHUD;
     [SerializeField] RectTransform playButton;
     [SerializeField] Transform logo;
+
+    private TextMeshProUGUI tutorialDescription;
     public void Awake()
     {
         cr = GameObject.FindWithTag("Player").GetComponent<CR>();
+        tutorialDescription = tutorialHUD.transform.Find("description").GetComponent<TextMeshProUGUI>();
     }
     public void GameStart()
     {
@@ -31,5 +37,25 @@ public class GameManager : MonoBehaviour
         Destroy(playButton.gameObject);
         cr.transform.DOMoveY(-3f, 1f).SetEase(Ease.OutCirc);
         Camera.main.DOOrthoSize(6.6f, 1f).SetEase(Ease.OutCirc);
+
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Tutorial());
+    }
+
+    public IEnumerator ChangeCanvasGroupAlpha(CanvasGroup group, float duration, float destination)
+    {
+        if (group == null) yield break;
+        float t = 0;
+        while ( t < duration )
+        {
+            t += Time.deltaTime;
+            group.alpha = t / duration * destination;
+            yield return null;
+        }
+    }
+    IEnumerator Tutorial()
+    {
+        tutorialDescription.text = $"방향키를 눌러 움직입니다";
+        yield return ChangeCanvasGroupAlpha(tutorialHUD, 1.2f, 1f);
     }
 }
